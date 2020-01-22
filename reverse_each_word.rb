8226996ccc160f1c1efa5993d65c3e3303bf3a57
Danny Soto <87dsoto@gmail.com>
2020-01-22 18:33:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   word_array = string.split(" ")
   rev_array = []
-  string.each do |word|
+  word_array.each do |word|
     rev_array.push(word.reverse)
   end
   return rev_array.join
