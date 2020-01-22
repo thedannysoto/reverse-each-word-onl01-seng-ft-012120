44422918b0147fb3e846d35d2449e910647d3167
Danny Soto <87dsoto@gmail.com>
2020-01-22 18:36:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ def reverse_each_word(string)
   word_array = string.split(" ")
   rev_array = []
   word_array.collect do |word|
-    word.reverse.join(" ")
+    word.reverse
   end
+  return word_array.join(" ")
   #return rev_array.join(" ")
 end
